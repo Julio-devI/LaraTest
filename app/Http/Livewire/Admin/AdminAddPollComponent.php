74b433e8501ad960a8eb43c88c988c4e00ab0353
julio <julioakzzo@hotmail.com>
2021-12-10 17:18:28 -0300

    create controller for criation poll

--- app/Http/Livewire/Admin/AdminAddPollComponent.php
+++ app/Http/Livewire/Admin/AdminAddPollComponent.php
@@ -2,10 +2,56 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Poll;
+use Carbon\Carbon;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class AdminAddPollComponent extends Component
 {
+    use WithFileUploads;
+
+    public $title;
+    public $poll_description;
+    public $image;
+    public $start_date;
+    public $end_date;
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'title' => 'required',
+            'poll_description' => 'required',
+            'image' => 'required|mimes:jpeg,png',
+            'start_date' => 'required',
+            'end_date' => 'required'
+        ]);
+    }
+
+    public function createNewPoll()
+    {
+        $this->validate([
+            'title' => 'required',
+            'poll_description' => 'required',
+            'image' => 'required|mimes:jpeg,png',
+            'start_date' => 'required',
+            'end_date' => 'required'
+        ]);
+
+        $npoll = new Poll();
+        $npoll->title = $this->title;
+        $npoll->poll_description = $this->poll_description;
+
+        $imageName = Carbon::now()->timestamp . '.' . $this->image->extension();
+        $this->image->storeAs('polls', $imageName);
+
+        $npoll->image = $imageName;
+        $npoll->start_date = $this->start_date;
+        $npoll->end_date = $this->end_date;
+        $npoll->save();
+        session()->flash('message', 'Poll has been saved successfully');
+    }
+
     public function render()
     {
         return view('livewire.admin.admin-add-poll-component')->layout('layouts.base');
--- resources/views/livewire/admin/admin-add-poll-component.blade.php
+++ resources/views/livewire/admin/admin-add-poll-component.blade.php
@@ -4,33 +4,45 @@
             <a href="{{route('admin.dashboard')}}" class="btn btn-info pull-right">All Polls</a>
         </div>
 
-        <form action="/events" method="POST" enctype="multipart/form-data">
+        @if(Session::has('message'))
+            <div class="alert alert-success" role="alert"> {{Session::get('message')}} </div>
+        @endif
+
+        <form class="form-horizontal" wire:submit.prevent="createNewPoll">
             @csrf
             <div class="form-group">
                 <label for="title"> Title</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="title for poll">
+                <input type="text" class="form-control" id="title" name="title" placeholder="title for poll" wire:model="title">
+                @error('title') <p class="text-danger">{{$message}}</p> @enderror
             </div>
     
             <div class="form-group">
                 <label for="date"> Start Date: </label>
-                <input type="date" class="form-control" id="start date" name="start date">
+                <input type="date" class="form-control" id="start date" name="start date" wire:model="start_date">
+                @error('start_date') <p class="text-danger">{{$message}}</p> @enderror
             </div>
     
             <div class="form-group">
                 <label for="title"> End Date </label>
-                <input type="date" class="form-control" id="end date" name="end date">
+                <input type="date" class="form-control" id="end date" name="end date" wire:model="end_date">
+                @error('end_date') <p class="text-danger">{{$message}}</p> @enderror
             </div>
     
             <div class="form-group">
                 <label for="title"> Description poll </label>
-                <textarea class="form-control" name="description poll" id="description poll" cols="30" rows="10"></textarea>
+                <textarea class="form-control" name="description poll" id="description poll" cols="20" rows="10" wire:model="poll_description"></textarea>
+                @error('poll_description') <p class="text-danger">{{$message}}</p> @enderror
             </div>
     
             <div class="form-group">
                 <label for="title"> Image poll </label>
-                <input type="file" class="form-control">
+                <input type="file" class="form-control-file" name="image" wire:model="image">
+                @error('image') <p class="text-danger">{{$message}}</p> @enderror
+                @if($image)
+                    <img src="{{$image->temporaryUrl()}}" width="60" alt="">
+                @endif
             </div>
 
-            <input type="submit" class="btn btn-primary" value="Create Poll">
+            <button type="submit" class="btn btn-success">Create Poll</button>
         </form>                         
 </div>
