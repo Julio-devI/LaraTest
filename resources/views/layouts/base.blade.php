3000fa7b403aa11cc02be50debf6a30bc6653afb
Júlio Alves <julio.a.valenca@gmail.com>
2021-12-10 12:01:58 -0300

    create component home

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -19,114 +19,58 @@
             <div class="tm-header-inner">
                 <a href="#" class="navbar-brand tm-site-name">Classic</a>
                     <!-- navbar -->
-                    <nav class="navbar tm-main-nav">
-                        <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#tmNavbar">
-                            &#9776;
-                        </button>
+                <nav class="navbar tm-main-nav">
+                    <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#tmNavbar">
+                        &#9776;
+                    </button>
                         
-                        <div class="collapse navbar-toggleable-sm" id="tmNavbar">
-                            <ul class="nav navbar-nav">
-                                <li class="nav-item active">
-                                    <a href="index.html" class="nav-link">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="about.html" class="nav-link">Séries</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="blog.html" class="nav-link">Fitness</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="contact.html" class="nav-link">Movies</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="contact.html" class="nav-link">Register</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="contact.html" class="nav-link">Login</a>
-                                </li>
-                            </ul>                        
-                        </div>
-                    </nav>  
-                </div>                                  
-            </div>            
-        </div>
-
-        <div class="tm-home-img-container">
-            <img src="{{asset('img/tm-home-img.jpg')}}" alt="Image" class="hidden-lg-up img-fluid">
-        </div>
-
-        <section class="tm-section">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
-
-                        <div class="tm-content-box">
-                            <img src="{{asset('img/tm-img-310x180-1.jpg')}}" alt="Image" class="tm-margin-b-20 img-fluid">
-                            <h4 class="tm-margin-b-20 tm-gold-text">Lorem ipsum dolor #1</h4>
-                            <p class="tm-margin-b-20">Aenean cursus tellus mauris, quis
-                            consequat mauris dapibus id. Donec
-                            scelerisque porttitor pharetra</p>
-                            <a href="#" class="tm-btn text-uppercase">Detail</a>    
-                        </div>  
-
+                    <div class="collapse navbar-toggleable-sm" id="tmNavbar">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item active">
+                                <a href="index.html" class="nav-link">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="about.html" class="nav-link">Séries</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="blog.html" class="nav-link">Fitness</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="contact.html" class="nav-link">Movies</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="contact.html" class="nav-link">Register</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="contact.html" class="nav-link">Login</a>
+                            </li>
+                        </ul>                        
                     </div>
+                </nav>  
+            </div>                                  
+        </div>            
+    </div>
 
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+    <div class="tm-home-img-container">
+        <img src="{{asset('img/tm-home-img.jpg')}}" alt="Image" class="hidden-lg-up img-fluid">
+    </div>
 
-                        <div class="tm-content-box">
-                            <img src="{{asset('img/tm-img-310x180-2.jpg')}}" alt="Image" class="tm-margin-b-20 img-fluid">
-                            <h4 class="tm-margin-b-20 tm-gold-text">Lorem ipsum dolor #2</h4>
-                            <p class="tm-margin-b-20">Aenean cursus tellus mauris, quis
-                            consequat mauris dapibus id. Donec
-                            scelerisque porttitor pharetra</p>
-                            <a href="#" class="tm-btn text-uppercase">Read More</a>    
-                        </div>  
-
-                    </div>
-
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
-
-                        <div class="tm-content-box">
-                            <img src="{{asset('img/tm-img-310x180-3.jpg')}}" alt="Image" class="tm-margin-b-20 img-fluid">
-                            <h4 class="tm-margin-b-20 tm-gold-text">Lorem ipsum dolor #3</h4>
-                            <p class="tm-margin-b-20">Aenean cursus tellus mauris, quis
-                            consequat mauris dapibus id. Donec
-                            scelerisque porttitor pharetra</p>
-                            <a href="#" class="tm-btn text-uppercase">Detail</a>     
-                        </div>  
-
-                    </div>
-
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
-
-                        <div class="tm-content-box">
-                            <img src="{{asset('img/tm-img-310x180-4.jpg')}}" alt="Image" class="tm-margin-b-20 img-fluid">
-                            <h4 class="tm-margin-b-20 tm-gold-text">Lorem ipsum dolor #4</h4>
-                            <p class="tm-margin-b-20">Aenean cursus tellus mauris, quis
-                            consequat mauris dapibus id. Donec
-                            scelerisque porttitor pharetra</p>
-                            <a href="#" class="tm-btn text-uppercase">Read More</a>    
-                        </div>  
-
-                    </div>
-                </div> <!-- row -->
-            </div>
-        </section>
+    {{$slot}}
         
-        <footer class="tm-footer">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-xs-12 tm-copyright-col">
-                        <p class="tm-copyright-text">Copyright 2021 julioDevI</p>
-                    </div>
+    <footer class="tm-footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12 tm-copyright-col">
+                    <p class="tm-copyright-text">Copyright 2021 julioDevI</p>
                 </div>
             </div>
-        </footer>
+        </div>
+    </footer>
 
-        <!-- load JS files -->
-        <script src="{{asset('js/jquery-1.11.3.min.js')}}"></script>             
-        <script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>  
-        <script src="{{asset('js/bootstrap.min.js')}}"></script>                 
-        @livewireScripts
+    <!-- load JS files -->
+    <script src="{{asset('js/jquery-1.11.3.min.js')}}"></script>             
+    <script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>  
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>                 
+    @livewireScripts
     </body>
 </html>
---
+++ resources/views/livewire/home-component.blade.php
@@ -0,0 +1,51 @@
+<div>
+    <section class="tm-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                    <div class="tm-content-box">
+                        <img src="{{asset('img/tm-img-310x180-1.jpg')}}" alt="Image" class="tm-margin-b-20 img-fluid">
+                        <h4 class="tm-margin-b-20 tm-gold-text">Lorem ipsum dolor #1</h4>
+                        <p class="tm-margin-b-20">Aenean cursus tellus mauris, quis
+                        consequat mauris dapibus id. Donec
+                        scelerisque porttitor pharetra</p>
+                        <a href="#" class="tm-btn text-uppercase">Detail</a>    
+                    </div>  
+                </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                    <div class="tm-content-box">
+                        <img src="{{asset('img/tm-img-310x180-2.jpg')}}" alt="Image" class="tm-margin-b-20 img-fluid">
+                        <h4 class="tm-margin-b-20 tm-gold-text">Lorem ipsum dolor #2</h4>
+                        <p class="tm-margin-b-20">Aenean cursus tellus mauris, quis
+                        consequat mauris dapibus id. Donec
+                        scelerisque porttitor pharetra</p>
+                        <a href="#" class="tm-btn text-uppercase">Read More</a>    
+                    </div>  
+                </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                    <div class="tm-content-box">
+                        <img src="{{asset('img/tm-img-310x180-3.jpg')}}" alt="Image" class="tm-margin-b-20 img-fluid">
+                        <h4 class="tm-margin-b-20 tm-gold-text">Lorem ipsum dolor #3</h4>
+                        <p class="tm-margin-b-20">Aenean cursus tellus mauris, quis
+                        consequat mauris dapibus id. Donec
+                        scelerisque porttitor pharetra</p>
+                        <a href="#" class="tm-btn text-uppercase">Detail</a>     
+                    </div>  
+                </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                    <div class="tm-content-box">
+                        <img src="{{asset('img/tm-img-310x180-4.jpg')}}" alt="Image" class="tm-margin-b-20 img-fluid">
+                        <h4 class="tm-margin-b-20 tm-gold-text">Lorem ipsum dolor #4</h4>
+                        <p class="tm-margin-b-20">Aenean cursus tellus mauris, quis
+                        consequat mauris dapibus id. Donec
+                        scelerisque porttitor pharetra</p>
+                        <a href="#" class="tm-btn text-uppercase">Read More</a>    
+                    </div>  
+                </div>
+            </div> <!-- row -->
+        </div>
+    </section>
+</div>
