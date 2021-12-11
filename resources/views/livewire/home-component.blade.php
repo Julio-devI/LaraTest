b0451334559917a1c9909b200f12bf1e29d9ade8
Júlio Alves <julio.a.valenca@gmail.com>
2021-12-10 23:29:13 -0300

    titulo

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1,5 +1,11 @@
 <div>
-    <h1 class="tm-gold-text" style="margin-left: 62px;">Recents polls</h1>
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+            <div class="tm-content-box">
+                <h1>Recents polls</h1>   
+            </div>
+        </div>
+    </div> <!-- row -->
     <section class="tm-section">
         <div class="container-fluid">
             <div class="row">
@@ -9,9 +15,9 @@
                             <img src="{{asset('images/polls')}}/{{$npoll->image}}" alt="Image" class="tm-margin-b-20 img-fluid">
                             <h4 class="tm-margin-b-20 tm-gold-text">{{$npoll->title}}</h4>
                             <p class="tm-margin-b-18 tm-gold-text">{{$npoll->poll_description}}</p>
-                            <p class="tm-margin-b-18 tm-gold-text">Data de inicio: {{$npoll->start_date}}</p>
-                            <p class="tm-margin-b-18 tm-gold-text">Data de termino: {{$npoll->end_date}}</p>
-                            <a href="#" class="tm-btn">Votos: 1000</a>   
+                            <p class="tm-margin-b-18">Data de inicio: {{$npoll->start_date}}</p>
+                            <p class="tm-margin-b-18">Data de termino: {{$npoll->end_date}}</p>
+                            <a href="#" class="tm-btn">Opcao A: 1000 votos</a>   
                         </div>
                     </div>
                 @endforeach
